5e81c77e3afe6987eee259b4176f7581c6ed5ff4
e195718 <e195718@ie.u-ryukyu.ac.jp>
2022-01-25 17:38:23 +0900

    コメントの追加と修正

--- heatmap.py
+++ heatmap.py
@@ -1,21 +1,14 @@
 import pandas as pd
 import numpy as np
-import sklearn.model_selection as cross_validation
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import learning_curve 
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import seaborn as sns
-from mlxtend.feature_selection import ExhaustiveFeatureSelector as EFS
 
+##訓練データとテストデータ、提出用サンプルデータの読み込み
 train = pd.read_table('train.tsv')
 test = pd.read_table('test.tsv')
 sample = pd.read_csv('sample_submit.csv', header=None)
 
-
+#ヒートマップを作成し表示する
 plt.figure(figsize=(11, 9))
 corr_matrix = train.corr()
 sns.heatmap(corr_matrix, annot=True,fmt='.2f',cmap='Blues',square=True)
