0d755af6eb7cf9e28b7f8c7c6dc7dbc6ee9b7e74
e195718 <e195718@ie.u-ryukyu.ac.jp>
2022-01-25 17:10:00 +0900

    コメントの修正

--- RandomForest.py
+++ RandomForest.py
@@ -1,35 +1,35 @@
 import pandas as pd
-import numpy as np
-import sklearn.model_selection as cross_validation
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import learning_curve 
-import matplotlib.pyplot as plt
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import seaborn as sns
 from mlxtend.feature_selection import ExhaustiveFeatureSelector as EFS
 from sklearn.model_selection import GridSearchCV
 
-
+#訓練データとテストデータ、提出用サンプルデータの読み込み
 train = pd.read_table('train.tsv')
 test = pd.read_table('test.tsv')
 sample = pd.read_csv('sample_submit.csv', header=None)
 
+#訓練データから目的変数を削除
 trainX = train.drop('bot', axis=1)
-trainX = trainX[['id', 'default_profile', 'default_profile_image', 'friends_count', 'followers_count', 'favourites_count', 'geo_enabled', 'listed_count', 'mean_mins_between_tweets', 'mean_tweet_length']]
+#EFSを用いて選ばれた特徴量を選択
+trainX = trainX[['default_profile', 'default_profile_image', 'friends_count', 'followers_count', 'favourites_count', 'geo_enabled', 'listed_count', 'mean_mins_between_tweets', 'mean_tweet_length']]
+#ラベル作成
 y = train['bot']
 
+#テストデータのコピー
 testX = test.copy()
 
+#アンダーサンプリング
 positive_count_train = y.value_counts()[1]
 strategy = {0:positive_count_train*2, 1:positive_count_train}
 rus = RandomUnderSampler(random_state=0, sampling_strategy = strategy)
 X_resampled, y_resampled = rus.fit_resample(trainX, y)
 y_resampled.value_counts()
-#EFSで特徴量選択
 
+#EFSで特徴量選択
 """
 clf = RandomForestClassifier()
 efs1 = EFS(clf, min_features=10, max_features=15)
@@ -38,19 +38,15 @@ print('Best accuracy score: %.2f' % efs1.best_score_)
 print('Best subset:', efs1.best_feature_names_)
 """
 
-testX = testX[['id', 'default_profile', 'default_profile_image', 'friends_count', 'followers_count', 'favourites_count', 'geo_enabled', 'listed_count', 'mean_mins_between_tweets', 'mean_tweet_length']]
-
+#テストデータと検証用データを8:2で分割
 X_train,X_test,y_train,y_test = train_test_split(trainX,y,test_size=0.20,random_state=1)
 
-
+#グリッドサーチで調整するパラメータ
+"""
 parameters = {  
     'n_estimators': [600, 650, 700],     # 用意する決定木モデルの数
     'max_depth': [5, 6, 7],     # 決定木のノード深さの制限値
 }
-
-
-clf = RandomForestClassifier(max_depth=7, max_features=None, n_estimators=600)                                   
-
 
 gridsearch = GridSearchCV(estimator = clf,        # モデル
                           param_grid = parameters,  # チューニングするハイパーパラメータ
@@ -58,23 +54,30 @@ gridsearch = GridSearchCV(estimator = clf,        # モデル
                           scoring = "f1"      # スコアリング
                          )   
 
+#グリッドサーチ実行し、ベストなパラメータの値を出力
 gridsearch.fit(X_train, y_train)
 print('Best params: {}'.format(gridsearch.best_params_)) 
 print('Best Score: {}'.format(gridsearch.best_score_))
+"""
 
+#グリッドサーチによって求めたパラメータを引数に入力したモデル
+clf = RandomForestClassifier(max_depth=7, max_features=None, n_estimators=600)                                   
 
-
+#学習
 clf.fit(X_train, y_train)
 
-# 学習データに対する精度
-y_pred = clf.predict(X_test) #テスト用データの予測
+#テスト用データの予測
+y_pred = clf.predict(X_test)
 
+#訓練データに対する実行結果
 trainaccuracy_random_forest = clf.score(X_train, y_train)
 print('TrainAccuracy: {}'.format(trainaccuracy_random_forest))
 
+#検証用データに対する実行結果
 accuracy = accuracy_score(y_test, y_pred)
 print('TestAccuracy: {}'.format(accuracy))
 
+#提出用データに対する予測と提出ファイル(.csv)作成
 pred = clf.predict(testX)
 sample[1] = pred
 sample.to_csv('undersumpling.csv', index=None, header=None)
