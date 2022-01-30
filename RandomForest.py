18439f0b9ff6a6772cabc7835a89025b735fef5a
e195718 <e195718@ie.u-ryukyu.ac.jp>
2022-01-30 13:01:19 +0900

    update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -22,12 +22,6 @@ y = train['bot']
 #テストデータの特徴量選択
 testX = test[['default_profile', 'default_profile_image', 'friends_count', 'followers_count', 'favourites_count', 'geo_enabled', 'listed_count', 'mean_mins_between_tweets', 'mean_tweet_length']]
 
-#アンダーサンプリング
-positive_count_train = y.value_counts()[1]
-strategy = {0:positive_count_train*2, 1:positive_count_train}
-rus = RandomUnderSampler(random_state=0, sampling_strategy = strategy)
-X_resampled, y_resampled = rus.fit_resample(trainX, y)
-y_resampled.value_counts()
 
 #EFSで特徴量選択
 """
@@ -40,6 +34,13 @@ print('Best subset:', efs1.best_feature_names_)
 
 #テストデータと検証用データを8:2で分割
 X_train,X_test,y_train,y_test = train_test_split(trainX,y,test_size=0.20,random_state=1)
+
+#アンダーサンプリング
+positive_count_train = y_train.value_counts()[1]
+strategy = {0:positive_count_train*2, 1:positive_count_train}
+rus = RandomUnderSampler(random_state=0, sampling_strategy = strategy)
+X_resampled, y_resampled = rus.fit_resample(X_train, y_train)
+y_resampled.value_counts()
 
 #グリッドサーチで調整するパラメータ
 """
@@ -64,20 +65,22 @@ print('Best Score: {}'.format(gridsearch.best_score_))
 clf = RandomForestClassifier(max_depth=7, max_features=None, n_estimators=600)                                   
 
 #学習
-clf.fit(X_train, y_train)
+clf.fit(X_resampled, y_resampled)
 
 #テスト用データの予測
 y_pred = clf.predict(X_test)
 
 #訓練データに対する実行結果
-trainaccuracy_random_forest = clf.score(X_train, y_train)
+trainaccuracy_random_forest = clf.score(X_resampled, y_resampled)
 print('TrainAccuracy: {}'.format(trainaccuracy_random_forest))
 
 #検証用データに対する実行結果
 accuracy = accuracy_score(y_test, y_pred)
 print('TestAccuracy: {}'.format(accuracy))
 
+"""
 #提出用データに対する予測と提出ファイル(.csv)作成
 pred = clf.predict(testX)
 sample[1] = pred
 sample.to_csv('undersumpling.csv', index=None, header=None)
+"""
